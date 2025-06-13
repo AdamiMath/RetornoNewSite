ac20d18c19bcf3b4fc1f872d2288846729238407
AdamiMath <matheusadami.lima@gmail.com>
2025-06-13 17:25:23 -0300

    parte subido

--- src/Components/Subido.jsx
+++ src/Components/Subido.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 
 const Subido = () => {
-  return (
+
+return (
     <div className='containerSubido'>
+      <div>
         <h3 className='h3subido'>Comunidade <strong>subido</strong> de tráfego</h3>
-        <p className='p-padrao'>   Fazemos parte da maior comunidade de tráfego pago do Brasil, 
+        <p className='p-subido'>   Fazemos parte da maior comunidade de tráfego pago do Brasil, 
           formada por profissionais que estão sempre atualizados com as estratégias que realmente funcionam. 
           Isso significa que seu negócio estará nas mãos de quem domina as melhores práticas do mercado digital.
         </p>
+      </div>
     </div>
   )
 }
